55f1eb39bcfa4ca591712736f634636e463f929f
HIGUCHI Daisuke <d-higuchi@creationline.com>
2014-10-17 11:10:06 +0900

    for busser-serverspec >= 5.0

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,2 +1,22 @@
 require 'serverspec'
-set :backend, :exec
+begin
+  set :backend, :exec
+rescue Exception
+  require 'pathname'
+  ### include requirements ###
+
+  include SpecInfra::Helper::Exec
+  include SpecInfra::Helper::DetectOS
+
+  RSpec.configure do |c|
+    if ENV['ASK_SUDO_PASSWORD']
+      require 'highline/import'
+      c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
+    else
+      c.sudo_password = ENV['SUDO_PASSWORD']
+    end
+    c.before :all do
+      c.os = backend(Serverspec::Commands::Base).check_os
+    end
+  end
+end
