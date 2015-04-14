96457d37c01670bd0ef3a289613ac1e222b4ccba
HIGUCHI Daisuke <d-higuchi@creationline.com>
2015-04-14 18:52:34 +0900

    fix typo

--- test/integration/default/serverspec/localhost/apache2-take_spec.rb
+++ test/integration/default/serverspec/localhost/apache2-take_spec.rb
@@ -19,7 +19,7 @@ end
   describe file( i ) do
     it { should be_file }
     it { should be_owned_by 'root' }
-    it { should be_grouped_into_by 'root' }
+    it { should be_grouped_into 'root' }
     it { should be_mode 644 }
   end
 end
